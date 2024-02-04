338201a5360346e2f0b9118559f34121d7200b03
Olorunfemi <Femibam1@gmail.com>
2024-02-04 14:49:12 -0500

    login/register working

--- src/components/auth /loginForm.js
+++ src/components/auth /loginForm.js
@@ -1,13 +1,54 @@
 "use client";
-import React from "react";
+import { auth } from "@/app/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import CustomInput from "../shared/customInput";
 
-
 const LoginForm = () => {
+  const [loginData, setLoginData] = useState({
+    emailAddress: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const handleChange = (e) => {
+    //Use e to retrive name and value of input field and then proceed to update login data
+    const { name, value } = e.target;
+    setLoginData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  console.log(loginData);
+  //after successfully logging in, it will direct user to application
+  const router = useRouter();
+
   const handleSubmit = (e) => {
+    //Preventing page from being refreshed and setting error to null
     e.preventDefault();
     console.log(e.target);
+    setError(null);
+
+    // Assuming loginData includes emailAddress and password
+    const { emailAddress, password } = loginData;
+
+    
+    
+    signInWithEmailAndPassword(auth, emailAddress, password)
+      //Using firebase to login user auth, email, and password.
+      .then((authUser) => {
+        console.log(authUser);
+
+        console.log("Success. The user is Logged In");
+        // router.push("/auth/login");
+      })
+      .catch((error) => {
+        console.log(error.message);
+        // An error occurred. Set an error message to be displayed to the user
+        setError(error.message);
+      });
   };
+
   return (
     <div className="bg-backgroundSecondary flex flex-col p-4 rounded-2xl shadow-2xl">
       <div>
@@ -22,17 +63,20 @@ const LoginForm = () => {
           placeholder="Enter your Email"
           name="emailAddress"
           label="Email Address"
+          handleChange={handleChange}
         />
         <CustomInput
           type="password"
           placeholder="Enter your Password"
           name="password"
           label="Password"
+          handleChange={handleChange}
         />
         <div className="flex items-center gap-4 justify-center">
           <input type="checkbox" name="remember" id="remember" />{" "}
           <label htmlFor="remember">Remember for 30 days</label>
         </div>
+        {error && <p className="text-red-600">{error}</p>}
 
         <button
           className="bg-buttonColor mt-2 rounded-lg hover:scale-105 hover:shadow-lg w-full py-2 text-white"
