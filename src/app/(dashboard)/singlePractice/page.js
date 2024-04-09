6a6199fa3a92a42313dc4c6d6f1f5ea08bb23d67
HRM0 <103372533+HRM0@users.noreply.github.com>
2024-04-09 11:41:34 -0400

    more refactoring

--- src/app/(dashboard)/singlePractice/page.js
+++ src/app/(dashboard)/singlePractice/page.js
@@ -6,7 +6,6 @@ import { doc, getDoc, updateDoc} from "firebase/firestore";
 import SaveNotification from './saveNotification';
 
 const Practice = () => {
-  header = "Practice"
   const [showSaveNotification, setShowSaveNotification] = useState(false);
   const searchParams = useSearchParams()
   const userId = searchParams.get('uid') ? searchParams.get('uid') : null;
@@ -66,22 +65,19 @@ const Practice = () => {
 
   return (
     <div className='flex flex-col px-4 flex-grow'>
-      <div className="flex justify-between bg-white p-8 rounded-xl mb-2">
-        <h1 className="font-extrabold text-primary text-6xl">{header}</h1>
-      </div>
       {showSaveNotification ? (
         <SaveNotification isOpen={showSaveNotification} onClose={() => setShowSaveNotification(false)} />
         ) : (
         <>
-          <div className="flex justify-between mb-4 ">
-            <button className="p-2 bg-primary rounded-md text-white text-lg font-medium" onClick={() => getLesson("last")}>Previous Lesson</button>
-            <button className="p-2 bg-primary rounded-md text-white text-lg font-medium" onClick={() => getLesson("next")}>Next Lesson</button>
-          </div>
           <div className="flex flex-col p-4 px-4 bg-white rounded-md mb-4">
             <div className="text-3xl font-bold text-center my-4 border-b-2 border-gray-500 pb-2">{lesson.title}</div>
             <div className="text-2xl font-semibold text-left my-3">{lesson.lessonTitle}</div>
             <div className="text-1xl font-semibold text-left my-3">{lesson.lesson["Topic"]}</div>
             <div>{lesson.lesson["What to study"]}</div>
+          </div>
+          <div className="flex justify-between mb-4 ">
+            <button className="p-2 bg-primary rounded-md text-white text-lg font-medium" onClick={() => getLesson("last")}>Previous Lesson</button>
+            <button className="p-2 bg-primary rounded-md text-white text-lg font-medium" onClick={() => getLesson("next")}>Next Lesson</button>
           </div>
           <div className="flex flex-col p-4 px-4 bg-white rounded-md ">
             <textarea
--- src/components/dashboard/header.js
+++ src/components/dashboard/header.js
@@ -3,14 +3,26 @@ import React from "react";
 import { useAuth } from "../context/AuthContext";
 
 const Header = () => {
-  const header =  window.location.pathname === "/"? "Welcome" : window.location.pathname.split("/").slice(1)[0][0].toUpperCase()+window.location.pathname.slice(2)
+  const getHeader = () => {
+    switch(window.location.pathname) {
+      case "/":
+        return "Welcome"
+        break;
+      case "/singlePractice":
+        return "Practice"
+        break;
+      default:
+        return window.location.pathname.split("/").slice(1)[0][0].toUpperCase()+window.location.pathname.slice(2)
+    }
+
+  } 
   const { user } = useAuth();
 
   return (
     <div className="flex flex-col px-4 py-5">
       <h3 className="mb-8"> Hello {user?.displayName}</h3>
       <div className="flex justify-between bg-white p-8 rounded-xl mb-2">
-        <h1 className="font-extrabold text-primary text-6xl">{header}</h1>
+        <h1 className="font-extrabold text-primary text-6xl">{getHeader()}</h1>
       </div>
     </div>
   );
